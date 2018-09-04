aad2ce805db280a21f5d8c6edf94af9bd8031c93
bodqhrohro <bodqhrohro@gmail.com>
2018-09-04 23:05:05 +0300

    ticket:1 temporary probe to check the static dir contents

--- apps/contact_page/views.py
+++ apps/contact_page/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 from .models import _model_to_tuple, _keyvalue_to_str, GeneralInfo, Contact
 
+from pprint import pprint
+import os
+
 
 KNOWN_CONTACT_TYPES = ['Email', 'Jabber', 'Skype']
 
@@ -35,6 +38,10 @@ def _add_multiline_flag(tuples_list):
 
 
 def contact_page(request):
+    try:
+        pprint(os.listdir('static'))
+    except (IOError, OSError):
+        pass
     general_info = map(_model_to_tuple, GeneralInfo.objects.all())
     contacts = map(_model_to_tuple, Contact.objects.all())
 
