ee18a922bb32b991f007907e52dc9039479178e9
bodqhrohro <bodqhrohro@gmail.com>
2018-09-04 20:57:03 +0300

    Revert "ticket:1 yet another temporary probe to check if the static is properly collected"
    This reverts commit d9180d78747cc2c437e1a5c4b7e0499ef539b0b1.

--- apps/contact_page/views.py
+++ apps/contact_page/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 
 from .models import _model_to_tuple, _keyvalue_to_str, GeneralInfo, Contact
 
-from pprint import pprint
-import os
 
 KNOWN_CONTACT_TYPES = ['Email', 'Jabber', 'Skype']
 
@@ -37,10 +35,6 @@ def _add_multiline_flag(tuples_list):
 
 
 def contact_page(request):
-    try:
-        pprint(os.listdir('.'))
-    except (IOError, OSError):
-        pass
     general_info = map(_model_to_tuple, GeneralInfo.objects.all())
     contacts = map(_model_to_tuple, Contact.objects.all())
 
