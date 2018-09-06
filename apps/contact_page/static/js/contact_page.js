b7e5bb2b553fcdc577a53cd6ec480762b0b8f445
bodqhrohro <bodqhrohro@gmail.com>
2018-09-06 23:47:42 +0300

    ticket:3 turn html mockup into django template with hard-coded data

---
+++ apps/contact_page/static/js/contact_page.js
@@ -0,0 +1,27 @@
+$(function() {
+    var $requestsIframe = $('#requests')
+    var requestsIframe = $requestsIframe.get(0)
+
+    // update the requests iframe every 10 seconds
+    var UPDATE_INTERVAL = 10
+    window.setInterval(function() {
+        requestsIframe.contentWindow.location.reload()
+    }, UPDATE_INTERVAL * 1000)
+
+    // listen to visited pages counter
+    var originalTitle = document.title
+    window.addEventListener('message', function(e) {
+        // security check
+        var iframeLocation = requestsIframe.contentWindow.location
+        if (
+            e.origin === iframeLocation.protocol + '//' + iframeLocation.host &&
+            e.data.type === 'new_requests_count'
+        ) {
+            if (e.data.value) {
+                document.title = '(' + e.data.value + ') ' + originalTitle
+            } else {
+                document.title = originalTitle
+            }
+        }
+    }, false)
+})
